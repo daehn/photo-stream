f4449adff30a5612c276e544d96e251daed0e7bd
Silvan Dähn <silvan.daehn@mercedes-benz.io>
2020-04-13 19:59:45 +0200

    Adjust sorting logic

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -5,8 +5,8 @@ module Jekyll
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
       photos.sort_by do |photo|
-        EXIFR::JPEG.new(photo.path).date_time.to_s
-      end.reverse
+        EXIFR::JPEG.new(photo.path).date_time_original.to_s
+      end
     end
   end
 end
